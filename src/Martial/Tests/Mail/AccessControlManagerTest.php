fed8c104bd8864eb3ce714141dd15572b7b80cc7
Martial Saunois <saunois.martial@gmail.com>
2014-07-01 14:48:47 +0200

    Add unit test for the method AccessControlManager::isAllowedToSend().
    A test was added to be sure that the sender is not the same user as the recipient.

--- src/Martial/Tests/Mail/AccessControlManagerTest.php
+++ src/Martial/Tests/Mail/AccessControlManagerTest.php
@@ -6,13 +6,37 @@ use Martial\Mail\AccessControlManager;
 
 class AccessControlManagerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var AccessControlManager
+     */
+    public $accessControlManager;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    public $senderMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    public $recipientMock;
+
+    public function setUp()
+    {
+        $this->accessControlManager = new AccessControlManager();
+        $this->senderMock = $this->getMock('\Martial\Mail\UserInterface');
+        $this->recipientMock = $this->getMock('\Martial\Mail\UserInterface');
+    }
+
     public function testIsAllowedToSend()
     {
-        $accessControlManager = new AccessControlManager();
-        $sender = $this->getMock('\Martial\Mail\UserInterface');
-        $recipient = $this->getMock('\Martial\Mail\UserInterface');
+        $accessGranted = $this->accessControlManager->isAllowedToSend($this->senderMock, $this->recipientMock);
+        $this->assertTrue($accessGranted);
+    }
 
-        $accessGranted = $accessControlManager->isAllowedToSend($sender, $recipient);
-        $this->assertSame(true, $accessGranted);
+    public function testIsAllowedToSendShouldReturnsFalseWhenTheSenderIsTheRecipient()
+    {
+        $accessGranted = $this->accessControlManager->isAllowedToSend($this->senderMock, $this->senderMock);
+        $this->assertFalse($accessGranted, 'The sender is the same user as the recipient');
     }
 }
