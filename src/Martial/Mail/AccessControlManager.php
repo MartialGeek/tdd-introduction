95b06b93620d39e9768458dabed741d1e76d43dd
Martial Saunois <saunois.martial@gmail.com>
2014-07-01 14:39:18 +0200

    Add unit test for the method AccessControlManager::isAllowedToSend()
    Basic test that returns always "true".

---
+++ src/Martial/Mail/AccessControlManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Martial\Mail;
+
+/**
+ * Class AccessControlManager
+ * @package Martial\Mail
+ */
+class AccessControlManager implements AccessControlInterface
+{
+    /**
+     * Check if a user can send a message to another user.
+     * @param UserInterface $sender
+     * @param UserInterface $recipient
+     * @return bool
+     */
+    public function isAllowedToSend(UserInterface $sender, UserInterface $recipient)
+    {
+        return true;
+    }
+
+    /**
+     * Check if a user can read a message from another user.
+     * @param UserInterface $sender
+     * @param UserInterface $recipient
+     * @return bool
+     */
+    public function isAllowedToRead(UserInterface $sender, UserInterface $recipient)
+    {
+        // TODO: Implement isAllowedToRead() method.
+    }
+}
